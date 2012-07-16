dd3f0ca0169252958acab33d592e7167d2bea21a
Sebastian Henschel <sebastian@transparency.ge>
2012-07-16 21:18:23 +0200

    Introduced new templatetag repdate to get around issues with ApproximateDateField and Django's date filter.

--- apps/representative/templatetags/representative.py
+++ apps/representative/templatetags/representative.py
@@ -2,6 +2,8 @@
 Representative templatetags
 """
 from django import template
+from django.utils import dateformat
+from django.utils.translation import ugettext_lazy as _
 
 register = template.Library()
 
@@ -18,3 +20,25 @@ def percentage (value, total):
         return '%.1f%%' % (value * 100. / total)
     else:
         return '0%'
+
+
+
+@register.filter
+def repdate (approx):
+    """Alternative to Django's date filter which doesn't play well with ApproximateDateFields.
+
+    @param approx: approximate date
+    @type: ApproximateDateField
+    @return: nicely formatted date
+    @rtype: str
+    """
+    if approx.future:
+        return _(u'future')
+    elif approx.year and approx.month and approx.day:
+        return dateformat.format(approx, 'j F Y').encode('utf-8')
+    elif approx.year and approx.month:
+        return dateformat.format(approx, 'F Y').encode('utf-8')
+    elif approx.year:
+        return dateformat.format(approx, 'Y').encode('utf-8')
+    else:
+        return _(u'invalid')
