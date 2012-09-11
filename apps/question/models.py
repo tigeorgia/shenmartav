2f1b1c0e27622a8a600ae2e1135d316d5b3457bc
Sebastian Henschel <sebastian@transparency.ge>
2012-09-11 08:35:01 +0200

    Changed send to parliament only when question is set public.
    Also changed sender address to contain representative slug.
    
    Issue #49

--- apps/question/models.py
+++ apps/question/models.py
@@ -6,15 +6,159 @@ Depends on representative.
 """
 __docformat__ = 'epytext en'
 
-import urllib2, copy, datetime
-
+import urllib, urllib2, copy, datetime
 from cms.models.pluginmodel import CMSPlugin
+from django.contrib.sites.models import Site
 from django.core.mail import EmailMessage
 from django.db import models
 from django.utils.translation import ugettext as _
 
 from settings import QUESTION_SMS_URL
 from representative.models import Representative
+
+
+
+#: map representative id @ shenmartav.ge to parliament.ge article id
+SHENMARTAV2PARLIAMENT = {
+    '15': '16', # Akaki Bobokhidze
+    '89': '96', # Akaki Minashvili
+    '2': '4', # Andro Alavidze
+    '16': '17', # Anzor Bolkvadze
+    '23': '36', # Archil Gegenava
+    '8': '10', # Armenak Bainduriani
+    '43': '123', # Aslan Tavdgiridze
+    '72': '79', # Avtandil Lekashvili
+    '103': '116', # Avtandil Sturua
+    '104': '118', # Azer Suleimanovi
+    '11': '13', # Badri Basishvili
+    '19': '19', # Bezhan Butskhrikidze
+    '132': '64', # Bezhan Khurtsidze
+    '46': '119', # Chiora Taktakishvili
+    '12': '11', # Davit Bakradze
+    '14': '15', # Davit Bezhuashvili
+    '125': '23', # Davit Chavchanidze
+    '37': '26', # Davit Darchiashvili
+    '86': '89', # Davit Makhniashvili
+    '92': '99', # Davit Nadashvili
+    '49': '126', # Davit Todradze
+    '95': '104', # Devi Ovashvili
+    '128': '60', # Dilar Khabuliani
+    '76': '83', # Dimitri Lortkipanidze
+    '135': '50', # Edisher Jalaghonia
+    '130': '62', # Ekaterine Kherkheulidze
+    '61': '76', # Eldar Kvernadze
+    '134': '52', # Elene Javakhadze
+    '79': '88', # Elguja Makaradze
+    '25': '37', # Emzar Gelashvili
+    '91': '98', # Enzel Mkoiani
+    '75': '82', # Gela Londaridze
+    '29': '43', # Gia Goguadze
+    '53': '130', # Gia Tortladze
+    '6': '2', # Giorgi Akhvlediani
+    '5': '7', # Giorgi Asanidze
+    '126': '24', # Giorgi Chelidze
+    '20': '33', # Giorgi Gabashvili
+    '44': '122', # Giorgi (Givi) Targamadze
+    '31': '40', # Giorgi Godabrelidze
+    '30': '44', # Giorgi Goguadze
+    '32': '45', # Giorgi Gugava
+    '54': '48', # Giorgi Imnadze
+    '56': '54', # Giorgi Kandelaki
+    '129': '61', # Giorgi Khakhnelidze
+    '87': '94', # Giorgi Meladze
+    '99': '107', # Giorgi Roinishvili
+    '107': '120', # Giorgi Talakhadze
+    '45': '121', # Giorgi Targamadze
+    '118': '131', # Giorgi Tsagareishvili
+    '122': '132', # Giorgi Tsereteli
+    '39': '31', # Gocha Enukidze
+    '69': '72', # Gocha Kuprava
+    '113': '112', # Gocha Shanidze
+    '48': '125', # Gocha Tevdoradze
+    '18': '18', # Goderdzi Bukia
+    '74': '81', # Gogi Liparteliani
+    '117': '21', # Guram Chakhvadze
+    '55': '53', # Guram Kakalashvili
+    '133': '49', # Guranda Jabua
+    '137': '47', # Harutiun Hovhanesyan
+    '115': '114', # Iasha Shervashidze
+    '114': '113', # Ioseb Shatberashvili
+    '109': '55', # Irakli Kavtaradze
+    '58': '57', # Irakli Kenchoshvili
+    '112': '111', # Isvakhan Shamilov
+    '84': '86', # Jaba Maghlakelidze
+    '13': '9', # Jondo Baghaturia
+    '4': '5', # Kakhaber Anjaparidze
+    '121': '30', # Kakhaber Dzagania
+    '96': '103', # Kakhaber Okriashvili
+    '105': '117', # Kakhaber Sukhishvili
+    '26': '39', # Kakha Getsadze
+    '62': '77', # Kandid Kvitsiani
+    '64': '68', # Karlo Kopaliani
+    '28': '42', # Khatuna Gogorishvili
+    '97': '102', # Khatuna Ochiauri
+    '7': '8', # Koba Badagadze
+    '127': '59', # Koba Khabazi
+    '70': '73', # Koba Kurdghelashvili
+    '94': '100', # Koba Nakopia
+    '68': '71', # Korneli Kukulava
+    '36': '25', # Lasha Damenia
+    '90': '97', # Lasha Mindeli
+    '52': None, # Lasha Tordia
+    '42': '137', # Levan Vepkhvadze
+    '3': '6', # Magdalina Anikashvili
+    '22': '34', # Mamuka Gachechiladze
+    '27': '41', # Mamuka Gogatishvili
+    '102': '110', # Mamuka Saneblidze
+    '41': '138', # Marika Verulashvili
+    '100': '108', # Merab Samadashvili
+    '85': '84', # Mikheil Machavariani
+    '120': '135', # Mikheil Tskitishvili
+    '136': '51', # Naul Janashia
+    '71': '78', # Nikoloz Laliashvili
+    '1': '3', # Nugzar Abulashvili
+    '40': '32', # Nugzar Ergemlidze
+    '123': '133', # Nugzar Tsiklauri
+    '131': '63', # Otar Khinikadze
+    '51': '128', # Otar Toidze
+    '34': '27', # Paata Davitaia
+    '73': '80', # Paata Lezhava
+    '66': '69', # Pavle Kublashvili
+    '81': '91', # Petre Mamradze
+    '119': '134', # Petre Tsiskarishvili
+    '50': '127', # Pridon Todua
+    '47': '124', # Ramaz Tedoradze
+    '9': '14', # Ramin Bayramov
+    '78': '87', # Rati Maisuradze
+    '101': '109', # Rati Samkurashvili
+    '57': '56', # Roland Kemularia
+    '108': '106', # Roland Pipia
+    '82': '92', # Roman Marsagishvili
+    '17': '20', # Romanoz Bzhalava
+    '59': '58', # Rusudan Kervalishvili
+    '111': '74', # Samson Kutateladze
+    '110': '66', # Sergo Kitiashvili
+    '93': '101', # Shalva Natelashvili
+    '80': '90', # Shota Malashkhia
+    '38': '29', # Tamaz Diasamidze
+    '60': '75', # Tamaz Kvachantiradze
+    '98': '105', # Tamaz Petriashvili
+    '116': '22', # Teimuraz Charkviani
+    '124': '136', # Teimuraz Tsurtsumia
+    '65': '67', # Temur Kokhodze
+    '106': '115', # Tengiz Shkhirtladze
+    '10': '12', # Vakhtang Balavadze
+    '83': '93', # Vakhtang Martoleki
+    '35': '28', # Vasil Davitashvili
+    '21': '35', # Zaal Gamtsemlidze
+    '24': '38', # Zaza Gelashvili
+    '33': '46', # Zaza Gulikashvili
+    '77': '85', # Zaza Madurashvili
+    '63': '65', # Zurab Kikaleishvili
+    '88': '95', # Zurab Melikishvili
+    '67': '70', # Zviad Kukava
+}
+
 
 
 class PublicManager (models.Manager):
@@ -131,6 +275,46 @@ class Question (models.Model):
         self.representative.save()
 
 
+    def _send_parliament (self):
+        """Send a question to parliament(arian)."""
+        try:
+            arid = SHENMARTAV2PARLIAMENT[str(self.representative.pk)]
+        except KeyError:
+            arid = None
+
+        if arid:
+            url = 'http://parliament.ge/index.php?option=com_dmaskinfopopup'
+            site = Site.objects.get_current()
+            sender = 'ask-' + self.representative.slug + '@' + site.domain
+            values = {
+                'mail' : sender,
+                'name' : self.first_name + ' ' + self.last_name,
+                'subj' : self.question[:32] + ' ...',
+                'mess' : self.question,
+                'arid': arid,
+                'adminemail' : '-1',
+                'extrafield' : '',
+                'extrafield2' : '',
+                'extrafield3' : '',
+                'component' : 'com_k2',
+            }
+            data = urllib.urlencode(values)
+
+            req = urllib2.Request(url, data)
+            try:
+                opened = urllib2.urlopen(req)
+                response = 'SENDER: ' + sender + '\n' +\
+                    'RESPONSE CODE: ' + str(opened.getcode()) + '\n' +\
+                    'RESPONSE CONTENT: ' + opened.read()
+            except urllib2.URLError, err:
+                response = str(err) + '\n\n' + url
+        else:
+            name = str(self.representative.name)
+            response = 'No arid @ parliament.ge for representative %s' % name
+
+        self.parliament_response = response
+
+
     def save (self, *args, **kwargs):
         try:
             saved = Question.objects.get(pk=self.pk)
@@ -138,6 +322,9 @@ class Question (models.Model):
                 self._notify_questioner()
         except Question.DoesNotExist:
             pass
+
+        if self.is_public and not self.parliament_response:
+            self._send_parliament()
 
         super(Question, self).save(*args, **kwargs)
 
