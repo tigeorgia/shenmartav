72bbda53987cd7134d45ddd671e401ba495162f7
Derek Dohler <derek@transparency.ge>
2013-06-21 10:50:58 +0400

    Began switching to fabric for deployment

---
+++ deployment/fabdeploysource.py
@@ -0,0 +1,8 @@
+from fabric.contrib.project import rsync_project
+import os
+
+def sync_source(remote):
+    exclusions = ["*.pyc","settings.py","distsettings.py","requirements.txt",
+                  ".git",".gitignore","fabfile.py",]
+
+    rsync_project(local_dir=os.getcwd()+u"/",remote_dir=remote,exclude=exclusions)
