1b4d6997fdc8bf8187d57dceb9a632f54a7de991
myparl <myparl@myparl-vm.(none)>
2013-09-24 17:27:32 +0400

    added new voting records system

---
+++ apps/votingrecord/migrations/0004_auto__add_field_votingrecord_kan_id_chars__add_field_votingrecordresul.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'VotingRecord.kan_id_chars'
+        db.add_column('votingrecord_votingrecord', 'kan_id_chars',
+                      self.gf('django.db.models.fields.CharField')(max_length=32, null=True),
+                      keep_default=False)
+
+        # Adding field 'VotingRecordResult.session'
+        db.add_column('votingrecord_votingrecordresult', 'session',
+                      self.gf('django.db.models.fields.IntegerField')(null=True),
+                      keep_default=False)
+
+    def backwards(self, orm):
+        # Deleting field 'VotingRecord.kan_id_chars'
+        db.delete_column('votingrecord_votingrecord', 'kan_id_chars')
+
+        # Deleting field 'VotingRecordResult.session'
+        db.delete_column('votingrecord_votingrecordresult', 'session')
+
+    models = {
+        'cms.cmsplugin': {
+            'Meta': {'object_name': 'CMSPlugin'},
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
+            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
+            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        'cms.placeholder': {
+            'Meta': {'object_name': 'Placeholder'},
+            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
+        },
+        'popit.organisation': {
+            'Meta': {'ordering': "['slug']", 'object_name': 'Organisation'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'ended': ('django_date_extensions.fields.ApproximateDateField', [], {'max_length': '10', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '300'}),
+            'started': ('django_date_extensions.fields.ApproximateDateField', [], {'max_length': '10', 'blank': 'True'}),
+            'summary': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'summary_en': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'summary_ka': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'popit.person': {
+            'Meta': {'ordering': "['slug']", 'object_name': 'Person'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_of_birth': ('django_date_extensions.fields.ApproximateDateField', [], {'max_length': '10', 'blank': 'True'}),
+            'date_of_death': ('django_date_extensions.fields.ApproximateDateField', [], {'max_length': '10', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'description_en': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'description_ka': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'representative.party': {
+            'Meta': {'ordering': "['slug']", 'object_name': 'Party', '_ormbases': ['popit.Organisation']},
+            'acronym': ('django.db.models.fields.CharField', [], {'max_length': '16'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'organisation_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['popit.Organisation']", 'unique': 'True', 'primary_key': 'True'}),
+            'url': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'representative.representative': {
+            'Meta': {'ordering': "['slug']", 'object_name': 'Representative', '_ormbases': ['popit.Person']},
+            'answered': ('django.db.models.fields.FloatField', [], {'default': '0', 'null': 'True'}),
+            'committee': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'committee_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'committee_ka': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'contact_address_phone': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'contact_address_phone_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'contact_address_phone_ka': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'education': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'education_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'education_ka': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'elected': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'elected_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'elected_ka': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'electoral_district': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'electoral_district_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'electoral_district_ka': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'expenses': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'expenses_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'expenses_ka': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'faction': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'faction_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'faction_ka': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'family_status': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'family_status_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'family_status_ka': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'gender': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'is_majoritarian': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'other_income': ('django.db.models.fields.FloatField', [], {'default': '0', 'null': 'True'}),
+            'party': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'representatives'", 'null': 'True', 'to': "orm['representative.Party']"}),
+            'person_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['popit.Person']", 'unique': 'True', 'primary_key': 'True'}),
+            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'pob': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'pob_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'pob_ka': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'property_assets': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'property_assets_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'property_assets_ka': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'salary': ('django.db.models.fields.FloatField', [], {'default': '0', 'null': 'True'}),
+            'terms': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'representatives'", 'blank': 'True', 'to': "orm['representative.Term']"}),
+            'unit': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'representatives'", 'null': 'True', 'to': "orm['representative.Unit']"})
+        },
+        'representative.term': {
+            'Meta': {'object_name': 'Term'},
+            'end': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'name_ka': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'start': ('django.db.models.fields.DateField', [], {})
+        },
+        'representative.unit': {
+            'Meta': {'object_name': 'Unit'},
+            'active_term': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'unit_active'", 'null': 'True', 'to': "orm['representative.Term']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'inactive_terms': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'unit_inactive'", 'blank': 'True', 'to': "orm['representative.Term']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'name_ka': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'parties': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'unit'", 'symmetrical': 'False', 'to': "orm['representative.Party']"}),
+            'short': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        },
+        'votingrecord.votingrecord': {
+            'Meta': {'ordering': "['-date', '-number']", 'object_name': 'VotingRecord'},
+            'amended_by': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'amending'", 'null': 'True', 'to': "orm['votingrecord.VotingRecord']"}),
+            'date': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'kan_id': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'kan_id_chars': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '512', 'null': 'True'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '512', 'null': 'True', 'blank': 'True'}),
+            'name_ka': ('django.db.models.fields.CharField', [], {'max_length': '512', 'null': 'True', 'blank': 'True'}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True'}),
+            'scrape_date': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '100'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'})
+        },
+        'votingrecord.votingrecordamendment': {
+            'Meta': {'object_name': 'VotingRecordAmendment'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True'}),
+            'record': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'amendments'", 'to': "orm['votingrecord.VotingRecord']"})
+        },
+        'votingrecord.votingrecordpluginconf': {
+            'Meta': {'object_name': 'VotingRecordPluginConf', 'db_table': "'cmsplugin_votingrecordpluginconf'", '_ormbases': ['cms.CMSPlugin']},
+            'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "u'Voting Records'", 'max_length': '32'})
+        },
+        'votingrecord.votingrecordresult': {
+            'Meta': {'ordering': "['-vote']", 'object_name': 'VotingRecordResult'},
+            'css': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'record': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'results'", 'to': "orm['votingrecord.VotingRecord']"}),
+            'representative': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'votingresults'", 'null': 'True', 'to': "orm['representative.Representative']"}),
+            'session': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'vote': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        }
+    }
+
+    complete_apps = ['votingrecord']
