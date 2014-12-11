fbfb11b47c0c9c3314a34e0b9c4d21fc9f476c96
Etienne Baque <etiennebaque@gmail.com>
2014-12-11 12:00:55 +0400

    Added the 3 most recent questions, at the bottom of the home page

---
+++ apps/question/templatetags/question_row.py
@@ -0,0 +1,11 @@
+from django import template
+from apps.question.models import *
+
+register = template.Library()
+
+@register.inclusion_tag('question/question_row.html')
+def get_three_questions():
+    three_questions = Question.answered.order_by('-date')[:3]
+    return {'questions': three_questions}
+
+
