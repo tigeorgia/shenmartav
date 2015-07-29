6752cebc65a0eef03f892c1194fc6f702cbc2070
GeorgeLubaretsi <gio@transparency.ge>
2015-07-29 17:19:10 +0400

    Fix representative detail table layout and add paginated votingrecord list

--- apps/representative/static/js/representative/votingrecords.js
+++ apps/representative/static/js/representative/votingrecords.js
@@ -1,10 +1,16 @@
 $('#filter-by-vote').change(function (){
-    var filter = $(this).find(":selected").attr('name')
+    var filter = $(this).find(":selected").attr('name');
+    var pane = $('.scroll-pane');
+    pane.jScrollPane();
+    var api = pane.data('jsp');
+
     $('#container-votingrecords tr').each(function (){
         if ($(this).attr('class') != filter){
             $(this).hide();
         } else {
             $(this).show();
         }
-    })
+    });
+
+    api.reinitialise();
 });
